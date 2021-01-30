f10036065a16df389fddcd4fd8883331127e6d76
ravener <ravener.anime@gmail.com>
2021-01-30 18:14:30 +0430

    stuff

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import Terminal from '../components/Terminal';
 
 export default function Home() {
   return (
@@ -13,6 +14,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
+
+
+        <Terminal />
 
         <p className={styles.description}>
           Get started by editing{' '}
